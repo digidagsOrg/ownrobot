02636d237f83cd679cb0f5d186c184666cb67b8d
digidagsOrg <107912351+digidagsOrg@users.noreply.github.com>
2024-06-24 17:15:14 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,15 +1,24 @@
 radio.onReceivedNumber(function (receivedNumber) {
     if (receivedNumber == 12) {
-    	
+        motion1()
     } else if (receivedNumber == 8) {
-    	
+        motion2()
     } else if (receivedNumber == 0) {
-    	
+        Ausgangsposition()
     }
+})
+input.onButtonPressed(Button.A, function () {
+    motion1()
 })
 function motion2 () {
 	
 }
+input.onButtonPressed(Button.AB, function () {
+    Ausgangsposition()
+})
+input.onButtonPressed(Button.B, function () {
+    motion2()
+})
 function Ausgangsposition () {
     Position1 = 90
     Position2 = 90
@@ -17,13 +26,13 @@ function Ausgangsposition () {
     ServoSlow.setServoPosition(2, Position2)
 }
 function motion1 () {
-    Position1 = ServoSlow.setServoAngle(1, 90, 10, 90, ServoSlow.ServoDirection.CW)
-    Position2 = ServoSlow.setServoAngle(2, 90, 10, 90, ServoSlow.ServoDirection.CW)
+    Position1 = ServoSlow.setServoAngle(1, 70, 10, Position1, ServoSlow.ServoDirection.CW)
+    basic.pause(500)
+    Position1 = ServoSlow.setServoAngle(1, 140, 10, Position1, ServoSlow.ServoDirection.CCW)
+    basic.pause(500)
+    Position1 = ServoSlow.setServoAngle(1, 70, 10, Position1, ServoSlow.ServoDirection.CW)
 }
 let Position2 = 0
 let Position1 = 0
 radio.setGroup(1)
 Ausgangsposition()
-basic.forever(function () {
-	
-})
